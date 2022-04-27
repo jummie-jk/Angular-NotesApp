b7b04d0c633ed67e4ccb9722e3a5861fb9328078
Femi Olowofoyeku <ronsdartist@gmail.com>
2022-04-27 17:18:48 +0100

    about to add new pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  // Bootstrap testing code below!
+  encapsulation: ViewEncapsulation.None,
+  styles: [`
+    .card.disabled {
+      opacity: 0.5;
+    }
+    .custom-header::after {
+      content: none;
+    }
+  `]
 })
 export class AppComponent {
   title = 'NB';
+  // Bootstrap testing code!
+  disabled = false;
 }
